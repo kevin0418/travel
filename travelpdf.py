cce61c978a996f45c83b56bead2f943527840cdb
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2025-04-08 21:16:08 +1000

    Update file
    Update file  travelpdf program

--- travelpdf.py
+++ travelpdf.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 # PDF 파일 경로
-pdf_file_path = "nztravel.pdf"
+pdf_file_path = "nztravel1.pdf"
 
 # PDF 뷰어 컴포넌트 사용
 pdf_viewer(pdf_file_path)
